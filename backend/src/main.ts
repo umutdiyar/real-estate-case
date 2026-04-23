979eeec7c2402e17e41084df9511f8524ef426e9
umutdiyar <umutdiyarbalci@gmail.com>
2026-04-23 21:30:20 +0300

    allow CORS for production frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: 'https://real-estate-web-case.vercel.app',
     credentials: true,
   });
 
